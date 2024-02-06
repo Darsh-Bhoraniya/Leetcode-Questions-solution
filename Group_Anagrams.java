340017edd3f212b18984931c4e5198bb891b5c9d
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-02-06 18:58:36 +0530

    06-02-2024

---
+++ Group_Anagrams.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
