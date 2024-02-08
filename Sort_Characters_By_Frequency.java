62cc8de091e9030a1b180af9b83b55e291c0355a
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-02-08 18:51:11 +0530

    07-02-2024

--- Sort_Characters_By_Frequency.java
+++ Sort_Characters_By_Frequency.java
@@ -1,7 +1,6 @@
 class Solution {
     public String frequencySort(String s) {
-         Map<Character, Integer> hm = new HashMap<>();
-        
+         Map<Character, Integer> hm = new HashMap<>();        
         for (char c : s.toCharArray()) {
             hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
