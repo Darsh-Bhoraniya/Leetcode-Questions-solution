9e9ed194f9e554db4643f2cc59cba53141a4da6c
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-03-03 12:29:34 +0530

    Add files via upload

---
+++ March/Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,29 @@
+public class Remove_Nth_Node_From_End_of_List {
+     // Length for given List
+     int len = 0;
+     ListNode temp = head;
+     while (temp != null) {
+         temp = temp.next;
+         len++;
+     }
+     // int len = lenght;
+
+     //Remove nth node from the List
+     ListNode node = head;
+     int pos = 1;
+     while (pos < len - n) {
+         node = node.next;
+         pos++;
+     }
+
+     if (len == n)
+         head = head.next;
+
+     else if (node.next == null)
+         return null;
+
+     else
+         node.next = node.next.next;
+
+     return head;
+}
