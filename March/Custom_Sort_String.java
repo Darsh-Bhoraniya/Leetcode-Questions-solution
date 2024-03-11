3a214bc83bf7cee946ade2dc74d5339cfc66effe
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-03-11 16:58:30 +0530

    Add files via upload

---
+++ March/Custom_Sort_String.java
@@ -0,0 +1,23 @@
+class Custom_Sort_String {
+    public String customSortString(String order, String s) {
+        int [] map = new int [26];
+        for (int i = 0; i < s.length(); i ++) {
+            map[s.charAt(i) - 'a'] ++;
+        }
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < order.length(); i ++) {
+            for (int j = 0; j < map[order.charAt(i) - 'a']; j ++) {
+                result.append(order.charAt(i));
+            }
+            map[order.charAt(i) - 'a'] = 0;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < map[i]; j++) {
+                result.append((char) ('a' + i));
+            }
+        }
+        return result.toString();
+    }
+}
