77b4ef3c3d89f0e68a6735f10213b18af56a62f8
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-03-14 19:27:20 +0530

    Add files via upload

---
+++ March/Binary_Subarrays_With_Sum.java
@@ -0,0 +1,18 @@
+public class Binary_Subarrays_With_Sum {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int totalCount = 0;
+        int sum = 0;
+        int prefixSum[] = new int[nums.length + 1];
+        prefixSum[0] = 1;
+        
+        for (int num : nums) {
+            sum += num;
+            if (sum >= goal) {
+                totalCount += prefixSum[sum - goal];
+            }
+            prefixSum[sum]++;
+        }
+        
+        return totalCount;
+    }
+}   
