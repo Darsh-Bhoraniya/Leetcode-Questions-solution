f342810fff92d9f27d50f193c34646b2eb7afdff
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-03-28 15:02:59 +0530

    Add files via upload

---
+++ March/Length_of_Longest_Subarray_With_at_Most_K_Frequency.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Length_of_Longest_Subarray_With_at_Most_K_Frequency {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int ans = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int l = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            mp.put(nums[r], mp.getOrDefault(nums[r], 0) + 1);
+            while (mp.get(nums[r]) > k) {
+                mp.put(nums[l], mp.get(nums[l]) - 1);
+                l++;
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+
+    }
+}
