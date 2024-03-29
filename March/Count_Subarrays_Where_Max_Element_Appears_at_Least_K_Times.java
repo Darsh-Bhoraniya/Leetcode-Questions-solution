71d366142473021784311d0f4257fa4ef38bf669
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-03-29 12:00:58 +0530

    Add files via upload

---
+++ March/Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times{
+    public long countSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> freq = new HashMap<>();
+        int maxi = Arrays.stream(nums).max().getAsInt();
+        int i = 0, j = 0;
+        long ans = 0;
+        while (j < n) {
+            freq.put(nums[j], freq.getOrDefault(nums[j], 0) + 1);
+            while (freq.getOrDefault(maxi, 0) >= k) {
+                ans += n - j;
+                freq.put(nums[i], freq.get(nums[i]) - 1);
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+}
