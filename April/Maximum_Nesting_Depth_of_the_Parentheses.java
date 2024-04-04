761e153e24876d2c789b9b692f4b9ff925be4d02
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-04-04 17:28:08 +0530

    Add files via upload

--- April/Maximum_Nesting_Depth_of_the_Parentheses.java
+++ April/Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -13,3 +13,26 @@ public class Maximum_Nesting_Depth_of_the_Parentheses {
         return max;
     }
 }
+
+
+
+//Using Stack 
+
+import java.util.Stack;
+
+class Solution {
+    public int maxDepth(String s) {
+        Stack<Character> stack = new Stack<>();
+        int maxDepth = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+                maxDepth = Math.max(maxDepth, stack.size());
+            } else if (c == ')') {
+                stack.pop();
+            }
+        }
+        return maxDepth;
+    }
+}
+
