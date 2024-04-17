d39fdff2ab06d56bda74f04f8b6d2109e063d9c9
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-04-17 11:30:43 +0530

    Add files via upload

--- April/Smallest_String_Starting_From_Leaf.java
+++ April/Smallest_String_Starting_From_Leaf.java
@@ -1,3 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 public class Smallest_String_Starting_From_Leaf {
     public String smallestFromLeaf(TreeNode root) {
         StringBuilder smallest = new StringBuilder();
