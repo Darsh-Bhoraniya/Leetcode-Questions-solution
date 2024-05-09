66ab9ccff5e2f6a392b26d52bdc89f8cd152acad
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-05-09 10:18:49 +0530

    Add files via upload

---
+++ May/Maximize_Happiness_of_Selected_Children.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Maximize_Happiness_of_Selected_Children{
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        long res = 0;
+        int n = happiness.length, j = 0;
+
+        for (int i = n - 1; i >= n - k; --i) {
+            happiness[i] = Math.max(happiness[i] - j++, 0);
+            res += happiness[i];
+        }
+
+        return res;
+    }
+}
