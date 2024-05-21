e895cb2095c3e486bc908df9e986ef413c989cbd
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-05-21 10:50:43 +0530

    Add files via upload

--- May/Subsets.java
+++ May/Subsets.java
@@ -1,15 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Subsets{
+public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> response = new ArrayList();
-        List<Integer> curr = new ArrayList();
         response.add(new ArrayList());
-        for(int i =0 ;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int n = response.size();
-            for(int j = 0;j<n ;j++){
+            for (int j = 0; j < n; j++) {
                 List<Integer> temp = new ArrayList(response.get(j));
                 temp.add(num);
                 response.add(temp);
