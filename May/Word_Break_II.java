ba8d94f95f16c2700983954f8c879d4d461b6f71
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-05-25 10:37:53 +0530

    Add files via upload

---
+++ May/Word_Break_II.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Word_Break_II {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        return helper(s, 0, set);
+    }
+
+    private List<String> helper(String str, int s, Set<String> set) {
+        List<String> valid = new ArrayList<>();
+        if (s == str.length()) {
+            valid.add("");
+        }
+        for (int e = s + 1; e <= str.length(); e++) {
+            String pre = str.substring(s, e);
+            if (set.contains(pre)) {
+                List<String> suff = helper(str, e, set);
+                for (String suf : suff) {
+                    valid.add(pre + (suf.equals("") ? "" : " ") + suf);
+                }
+            }
+        }
+        return valid;
+    }
+}
