1712febfefb3c6dbfa41391cc11394033371aa27
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-06-01 20:18:02 +0530

    Update Score_of_a_String.java

---
+++ June/Score_of_a_String.java
@@ -0,0 +1,9 @@
+public class Score_of_a_String{
+  public int scoreOfString(String s) {
+            int res = 0;
+          for (int i = 0; i < s.length() - 1; i++) {
+              res += Math.abs(s.charAt(i) - s.charAt(i + 1));
+          }
+          return res;
+      }
+}
