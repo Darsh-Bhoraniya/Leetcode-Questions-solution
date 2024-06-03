cb92300beecd0b38cefdb8f2341aca4900519d16
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-06-03 18:40:10 +0530

    Add files via upload

---
+++ June/Append_Characters_to_String_to_Make_Subsequence.java
@@ -0,0 +1,15 @@
+public class Append_Characters_to_String_to_Make_Subsequence {
+     public int appendCharacters(String s, String t) {
+         int sIndex = 0, tIndex = 0;
+        int sLength = s.length(), tLength = t.length();
+        
+        while (sIndex < sLength && tIndex < tLength) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                tIndex++;
+            }
+            sIndex++;
+        }
+        
+        return tLength - tIndex;
+    }
+}
