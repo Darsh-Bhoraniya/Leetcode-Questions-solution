35f47adbfd25187376bfeaed346a2c605937c271
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-06-10 17:44:26 +0530

    Add files via upload

---
+++ June/Height_Checker.java
@@ -0,0 +1,16 @@
+public class Height_Checker {
+    public int heightChecker(int[] heights) {
+        int[] expected = new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            expected[i]=heights[i];
+        }
+        Arrays.sort(expected);
+        int count=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
