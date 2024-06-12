ecc109033b91074fe179f8e03f2b9eeb5e84d422
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-06-12 16:19:49 +0530

    Add files via upload

---
+++ June/Sort_Colors.java
@@ -0,0 +1,15 @@
+class Sort_Colors {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] > nums[j]) {
+                    // Swap nums[i] and nums[j]
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+}
