28601ea17d17153a664784b900db64d493f213b7
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-06-13 17:14:57 +0530

    Add files via upload

---
+++ June/Minimum_Number_of_Moves_to_Seat_Everyone.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Minimum_Number_of_Moves_to_Seat_Everyone {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int sum=0;
+        for(int i=0;i<seats.length;i++){
+            sum += Math.abs(seats[i]-students[i]);
+        }
+        return sum;
+    }
+}
