d02c48c700592aacd395aee6a95fa492063c4c24
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-06-14 21:32:23 +0530

    Add files via upload

---
+++ June/Minimum_Increment_to_Make_Array_Unique.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Minimum_Increment_to_Make_Array_Unique {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                ans += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return ans;
+    }
+}
