d2fbef703a2177b68748cb815e8c31fbc0a3490c
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-06-24 15:40:30 +0530

    Add files via upload

---
+++ June/Minimum_Number_of_K_Consecutive_Bit_Flips.java
@@ -0,0 +1,18 @@
+public class Minimum_Number_of_K_Consecutive_Bit_Flips {
+    public int minKBitFlips(int[] nums, int k) {
+        int n = nums.length, flipped = 0, res = 0;
+        int[] isFlipped = new int[n];
+        for (int i = 0; i < nums.length; ++i) {
+            if (i >= k)
+                flipped ^= isFlipped[i - k];
+                if (flipped == nums[i]) {
+                    if (i + k > nums.length)
+                        return -1;
+                isFlipped[i] = 1;
+                flipped ^= 1;
+                res++;
+            }
+        }
+        return res;
+    }
+}
