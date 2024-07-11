617e11a10801529ab7fb6c7954c005d8f840901e
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-07-11 11:11:21 +0530

    Add files via upload

---
+++ July/Reverse_Substrings_Between_Each_Pair_of_Parentheses.java
@@ -0,0 +1,23 @@
+public class Reverse_Substrings_Between_Each_Pair_of_Parentheses {
+    public String reverseParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (char currentChar : s.toCharArray()) {
+            if (currentChar == '(') 
+                stack.push(result.length());
+            else if (currentChar == ')') {
+                int start = stack.pop();
+                reverse(result, start, result.length() - 1);
+            } else result.append(currentChar);
+        }
+        return result.toString();
+    }
+    private void reverse(StringBuilder sb, int start, int end) {
+        while (start < end) {
+            char temp = sb.charAt(start);
+            sb.setCharAt(start++, sb.charAt(end));
+            sb.setCharAt(end--, temp);
+        }
+    }
+}
