d4a67a9763df11ad113149ddd0439d5d70f60ebe
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-08-01 16:56:00 +0530

    Add files via upload

---
+++ August/Number_of_Senior_Citizens.java
@@ -0,0 +1,12 @@
+public class Number_of_Senior_Citizens {
+    public int countSeniors(String[] s) {
+        int ans=0;
+        for(String ss:s){
+            int a1=ss.charAt(11)-'0';
+            int a2=ss.charAt(12)-'0'; 
+            if((a1*10)+a2>60)
+                ans++;
+        }
+        return ans;
+    }
+}
