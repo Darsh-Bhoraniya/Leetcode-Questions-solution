3d2c346db28b330d41d472f1839ba0d9dbd422f8
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-08-15 09:20:08 +0530

    Add files via upload

---
+++ August/Lemonade_Change.java
@@ -0,0 +1,22 @@
+public class Lemonade_Change {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                five--;
+                ten++;
+            } else if (ten > 0) {
+                five--;
+                ten--;
+            } else {
+                five -= 3;
+            }
+            if (five < 0) {
+                return false;
+            }
+        }
+            return true;
+    }
+}
