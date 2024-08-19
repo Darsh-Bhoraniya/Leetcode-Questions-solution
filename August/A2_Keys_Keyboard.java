872e1ad91517aeb2522cac04c863c2e439105cee
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-08-19 10:07:58 +0530

    Add files via upload

---
+++ August/A2_Keys_Keyboard.java
@@ -0,0 +1,16 @@
+public class 2_Keys_Keyboard{
+    public int minSteps(int n) {
+        int step=0;
+        int listsize=1;
+        int lastcopied=0;
+        while(listsize<n){
+            if(n%listsize==0){
+                lastcopied=listsize;     //copy
+                step++;
+            }
+            listsize+=lastcopied;      //paste
+            step++;
+        }
+        return step;
+    }
+}
