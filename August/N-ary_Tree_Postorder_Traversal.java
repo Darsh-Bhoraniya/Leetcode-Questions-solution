1e03047f28db8c8be9c170903f84a13fae11bdf2
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-08-26 17:54:38 +0530

    Add files via upload

---
+++ August/N-ary_Tree_Postorder_Traversal.java
@@ -0,0 +1,23 @@
+public class N_ary_Tree_Postorder_Traversal {
+    public List<Integer> postorder(Node root) {
+        // If the root is null, return an empty list
+        if (root == null) return new ArrayList<>();
+
+        List<Integer> res = new ArrayList<>();
+
+        // Start DFS from the root
+        dfs(root, res);
+
+        // Return the result list containing node values in post-order
+        return res;
+    }
+
+    private void dfs(Node root, List<Integer> res) {
+        // Recursively call dfs for each child of the current node
+        for (Node child : root.children) {
+            dfs(child, res);
+        }
+        // Append the value of the current node to the result list
+        res.add(root.val);
+    }
+}
