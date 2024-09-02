2db72965292791c90739584503bfbb5ebaac4e5c
Darsh Bhoraniya <103194778+Darsh-Bhoraniya@users.noreply.github.com>
2024-09-02 14:51:26 +0530

    Add files via upload

---
+++ Septmber/Find_the_Student_that_Will_Replace_the_Chalk.java
@@ -0,0 +1,19 @@
+public class Find_the_Student_that_Will_Replace_the_Chalk {
+    public int chalkReplacer(int[] chalk, int k) {
+        long totalChalkNeeded = 0;
+        for (int studentChalkUse : chalk) {
+            totalChalkNeeded += studentChalkUse;
+        }
+        
+        int remainingChalk = (int)(k % totalChalkNeeded);
+        
+        for (int studentIndex = 0; studentIndex < chalk.length; studentIndex++) {
+            if (remainingChalk < chalk[studentIndex]) {
+                return studentIndex;
+            }
+            remainingChalk -= chalk[studentIndex];
+        }
+        
+        return 0;  
+    }
+}
